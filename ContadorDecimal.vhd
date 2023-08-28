fb4367714b2272113e70b49e21787e3997c97f39
Douglas Moura <104034686+Douglas-code-eng@users.noreply.github.com>
2023-08-28 19:36:52 -0300

    Melhorias na Visibilidade

--- ContadorDecimal.vhd
+++ ContadorDecimal.vhd
@@ -9,17 +9,17 @@ entity ContadorDecimal is
 		reset: in std_logic;
 		s_reset: out std_logic;
 		clk : in std_logic;
-		s_decimal: out std_logic_vector (6 downto 0);
-		s_unidade: out std_logic_vector (6 downto 0);
-		s_dezena: out std_logic_vector (6 downto 0);
 		s_bcd7s_dezena: out std_logic_vector (0 to 6);
 		s_bcd7s_unidade: out std_logic_vector (0 to 6);
-		s_bcd: out std_logic_vector (7 downto 0)
+		s_bcd: out std_logic_vector (7 downto 0);
+		s_decimal: out std_logic_vector (6 downto 0);
+		s_unidade: out std_logic_vector (6 downto 0);
+		s_dezena: out std_logic_vector (6 downto 0)
 		
 	);
 end ContadorDecimal;
 
-architecture A of ContadorDecimal is
+architecture Cont of ContadorDecimal is
 
 component CLOCK1HZ
 	port (clkin : in std_logic;
@@ -34,10 +34,12 @@ signal unidade: std_logic_vector (6 downto 0);
 --signal sentido_contagem: std_logic := '0';-- Quando 0 é pra somar, quando 1 é pra subtrair
 signal clock: std_logic;
 begin
-s_reset <= clock;
-s_decimal <= q;
-s_dezena <= dezena;
-s_unidade <= unidade;
+
+	s_reset <= clock;
+	s_decimal <= q;
+	s_dezena <= dezena;
+	s_unidade <= unidade;
+	
 with dezena select
 	s_bcd7s_dezena <= "0000001" when "0000000",
 			"1001111" when "0000001",
@@ -63,105 +65,72 @@ with unidade select
 			"0001100" when "0001001",
 			"1111110" when others;
 			
-			
+			'
 	clkdiv: CLOCK1HZ PORT MAP (clk, clock);		
 	contar: process(clock, reset)
 	begin
-		if (reset = '0' and sentido_contagem = '0') then
-			q <= "0000000";
-			dezena <= "0000000";
-			unidade <= "0000000";
-			--sentido_contagem <= '0';
-		elsif (reset = '0' and sentido_contagem = '1') then
-			q <= "1100011";
-			dezena <= "0001001";
-			unidade <= "0001001";
-			--sentido_contagem <= '0';
-			-- use clk para a simulação
+		if (reset = '0') then  
+			if (sentido_contagem = '0') then
+				q <= (others=>"0");
+				dezena <= (others=>"0");
+				unidade <= (others=>"0");
+				--sentido_contagem <= '0';
+			else 
+				q <= "1100011";
+				dezena <= "0001001";
+				unidade <= "0001001";
+				--sentido_contagem <= '1';
+				
+			end if;
+			
 		elsif clock'event AND clock='0' then
 		
 			if sentido_contagem = '0' then
-				if (q >= 89) then
-					dezena <= "0001001";
-					unidade <= q - 89;
-				elsif (q >= 79) then
-					dezena <= "0001000";
-					unidade <= q - 79;
-				elsif (q >= 69) then
-					dezena <= "0000111";
-					unidade <= q - 69;				
-				elsif (q >= 59) then
-					dezena <= "0000110";
-					unidade <= q - 59;				
-				elsif (q >= 49) then
-					dezena <= "0000101";
-					unidade <= q - 49;				
-				elsif (q >= 39) then
-					dezena <= "0000100";
-					unidade <= q - 39;				
-				elsif (q >= 29) then
-					dezena <= "0000011";
-					unidade <= q - 29;				
-				elsif (q >= 19) then
-					dezena <= "0000010";
-					unidade <= q - 19;				
-				elsif (q >= 9) then
-					dezena <= "0000001";
-					unidade <= q - 9;
-				else
-					dezena <= "0000000";
-					unidade <= q+1;
-				end if;
+				for i in 8 downto 0 loop
+					if q >= i * 10 + 9 then
+						dezena <= "000" & std_logic_vector(to_unsigned(i, dezena'length));
+						unidade <= q - (i * 10 + 9);
+						exit; -- Sai da loop
+					end if;
+				end loop;
+
+				 dezena <= (others => '0');
+				 unidade <= q + 1;
+				 
+			end if;
 				
-				q <= q + 1;
-				if q = 99 then
-					--sentido_contagem <= '1';
-					q <= "0000000";
-					dezena <= "0000000";
-					unidade <= "0000000";
-				end if;
+			q <= q + 1;
+			
+			if q = 99 then
+				--sentido_contagem <= '1';
+				q <= (others=>"0");
+				dezena <= (others=>"0");
+				unidade <= (others=>"0");
+			end if;
+			
 			elsif sentido_contagem = '1' then
-				if (q >= 91) then
-					dezena <= "0001001";
-					unidade <= q - 91;
-				elsif (q >= 81) then
-					dezena <= "0001000";
-					unidade <= q - 81;
-				elsif (q >= 71) then
-					dezena <= "0000111";
-					unidade <= q - 71;				
-				elsif (q >= 61) then
-					dezena <= "0000110";
-					unidade <= q - 61;				
-				elsif (q >= 51) then
-					dezena <= "0000101";
-					unidade <= q - 51;				
-				elsif (q >= 41) then
-					dezena <= "0000100";
-					unidade <= q - 41;				
-				elsif (q >= 31) then
-					dezena <= "0000011";
-					unidade <= q - 31;				
-				elsif (q >= 21) then
-					dezena <= "0000010";
-					unidade <= q - 21;				
-				elsif (q >= 11) then
-					dezena <= "0000001";
-					unidade <= q - 11;
-				else
-					dezena <= "0000000";
-					unidade <= q - 1;
-				end if;
-				
+				 for i in 0 to 9 loop
+					  if q >= 10*(9-i)+1 then
+							dezena <= "000" & std_logic_vector(to_unsigned(i, dezena'length));
+							unidade <= q - 10*(9-i);
+							exit; -- Sai do loop
+					  end if;
+				 end loop;
+
+				 dezena <= (others => '0'); 
+				 unidade <= q - 1;
+			end if;
+							
 				q <= q - 1;
+				
 				if q = 0 then
 					--sentido_contagem <= '0';
 					q <= "1100011";
 					dezena <= "0001001";
-					unidade <= "0001001";
+					unidade <= "0001001";		
 				end if;
 			end if;
 		end if;
 	
 	end process contar;
-end A;
+end Cont;
